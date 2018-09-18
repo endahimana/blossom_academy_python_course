7a4266a3f9085f8ebff8245bb414207a50faa824
mrneont <neon.taylor@gmail.com>
2018-09-18 10:29:53 +0000

    importing

---
+++ _downloads/prog_file_01.py
@@ -0,0 +1,33 @@
+# Example program for importing
+# *yes* using __name__
+
+import numpy as np
+
+
+def func01_max(x, y):
+    '''func01_max(x, y)
+
+    This program takes two inputs and compares them to find
+    the maximum.  
+
+    The maximum value is output; if they are equal, 0 is returned.'''
+    
+    if x > y:
+        print("first value is greater")
+        return x
+    elif y > x:
+        print("second value is greater")
+        return y
+    else:
+        print("Equality!")
+        return 0
+
+# ----------------- end of definitions -------------------------------
+
+if __name__ == "__main__":
+
+    out1 = func01_max(5, 55)
+    print("In first comparison, the winner is:", out1)
+    
+    out2 = func01_max("a", "A")
+    print("comparing small/large letters, the winner is:", out2)
